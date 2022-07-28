7c7404d4fb2749f83589d19095a5f74e7b7c2f04
SoySamaQAQ <106502882+SoySamaQAQ@users.noreply.github.com>
2022-07-28 16:20:50 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hello Git");
         System.out.println("hello world");
         System.out.println("update");
+        System.out.println("pull update");
     }
 
 }
